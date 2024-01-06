558eee93557b27e37cc2722c51a5ac131f5ebbd8
catosaurusrex2003 <mohdmehdi2003@gmail.com>
2024-01-06 23:00:27 +0530

    yess

--- server.js
+++ server.js
@@ -6,11 +6,19 @@ const app = express();
 
 // Define a route handler for the default home page
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+
+    const moment = require('moment');
+    const formattedDateTime = moment().format('YYYY-MM-DD HH:mm:ss');
+    console.log("got a request", formattedDateTime)
+    res.send('Hello World!');
 });
 
 // Start the server on port 3000 and listen on all network interfaces
 const PORT = 3000;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
+// app.listen(PORT, '0.0.0.0', () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
